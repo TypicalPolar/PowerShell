0320fc77a2e72c318d3efc1235e0be6265de614b
TypicalPolar <josepartida.rh@gmail.com>
2021-04-01 11:25:22 -0700

    Recreate loop updated

--- folderrecreateLoop.ps1
+++ folderrecreateLoop.ps1
@@ -1,5 +1,30 @@
-$directoryList = Get-ChildItem -Recurse -Directory
+$directoryList = Get-ChildItem -Directory
 
 foreach ($directory in $directoryList){
-	Rename-Item $directory.fullname ($directory.fullname + old)
+    # Renaming old folder
+    Write-Progress -Activity 'Recreating Directory' -Status 'Renaming old folder' -PercentComplete 0
+    $oldPath = ($directory.fullname + '.old')
+	Rename-Item $directory.fullname $oldPath
+
+    # Creating new folder
+    Write-Progress -Activity 'Recreating Directory' -Status 'Creating new folder' -PercentComplete 25
+    New-Item -ItemType "directory" -Path $directory.fullname
+
+    # Transferring Content
+    Write-Progress -Activity 'Recreating Directory' -Status 'Migrating to new folder' -PercentComplete 50
+    Get-ChildItem -Path ($oldPath + '\*') -Recurse | Move-Item -Destination $directory.fullname
+
+    # Checking for unmoved files and Cleanup
+    Write-Progress -Activity 'Recreating Directory' -Status 'Removing old folder' -PercentComplete 75
+    $isEmpty = Get-ChildItem $oldPath | Measure-Object
+    if ( $isEmpty.Count -eq 0 ){
+    Write-Output "Successfully moved: $directory.fullname"
+    Remove-Item $oldPath
+    }
+    else{
+    Write-warning "Failure to move content: $directory.fullname  (Count Count = $isEmpty )"
+    }
+
+    # Complete
+    Write-Progress -Activity 'Recreating Directory' -Status 'Complete' -PercentComplete 100
 }
