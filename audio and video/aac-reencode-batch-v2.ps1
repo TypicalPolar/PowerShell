3b51f394e2608d6f5fc6a152142452e674bb1673
TypicalPolar <josepartida.rh@gmail.com>
2025-04-17 20:42:49 -0700

    Added initial format-trackcommand

--- audio and video/aac-reencode-batch-v2.ps1
+++ audio and video/aac-reencode-batch-v2.ps1
@@ -29,6 +29,75 @@ function Get-AudioInfo {
 
 }
 
+function Format-TrackCommand {
+    param (
+        [System.Object]
+        $Track
+    )
+
+    if($Track.Channels -in @(1,2,3,6,7,8)){
+        $Channels = $Track.Channels
+    }else{
+        # Stereo is fallback
+        $Channels = 2
+    }
+
+    $Settings = switch ($Channels) {
+        "1" {
+            @{
+                "ac" = 1
+                "layout" = "1.0"
+                "bitrate" = "128k"
+            }
+            break
+        }
+        "2" {
+            @{
+                "ac" = 2
+                "layout" = "2.0"
+                "bitrate" = "320k"
+            }
+            break
+        }
+        "3" {
+            @{
+                "ac" = 3
+                "layout" = "2.1"
+                "bitrate" = "320k"
+            }
+            break
+        }
+        "6" {
+            @{
+                "ac" = 6
+                "layout" = "5.1"
+                "bitrate" = "640k"
+            }
+            break
+        }
+        "7" {
+            @{
+                "ac" = 6
+                "layout" = "5.1"
+                "bitrate" = "640k"
+            }
+            break
+        }
+        "8" {
+            @{
+                "ac" = 8
+                "layout" = "7.1"
+                "bitrate" = "896k"
+            }
+            break
+        }
+    }
+
+    return $Settings
+        
+}
+
 # For Testing
 $File = $Queue[0].FullName
-Get-AudioInfo -File $File
+$TestAudioInfo = Get-AudioInfo -File $File
+Format-TrackCommand -Track $TestAudioInfo[1]
