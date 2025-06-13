ebb5a74b11773074b03d27be8f2f1dbd4c448387
TypicalPolar <josepartida.rh@gmail.com>
2025-06-12 20:47:03 -0700

    finished monitor flow and added sleep to fix break out issue

--- networking/PortDiscoveryMonitor/PortDiscoveryMonitor.ps1
+++ networking/PortDiscoveryMonitor/PortDiscoveryMonitor.ps1
@@ -1,3 +1,7 @@
+param(
+    [switch]$Monitor
+)
+
 $Directory = "C:\Tools\PortDiscoveryMonitor\"
 $LogDirectory = Join-Path -Path $Directory -ChildPath "Logs"
 $CurrPorts = Join-Path -Path $Directory -ChildPath "\cports\cports.exe"
@@ -16,17 +20,19 @@ function Get-CurrPorts {
 
 function Watch-ListeningPorts {
 
-    #/CaptureTime "54000" `
     $Timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
     $LogFile = Join-Path -Path $LogDirectory -ChildPath ($Timestamp + ".xml")
     & $CurrPorts /sxml $LogFile `
     /StartAsHidden `
-    /CaptureTime "54000" `
+    /CaptureTime "900000" `
     /DisplayIPv6Ports 0 `
     /DisplayListening 1 `
     /DisplayEstablished 0 `
     /DisplayNoState 0 `
     /RunAsAdmin `
+
+    # CurrPorts breaks out so PowerShell doesn't know to wait
+    Start-Sleep -Seconds 902
 
     return $LogFile
     
@@ -86,24 +92,38 @@ function Set-ScheduledTask {
 
 if($Monitor){
 
+    Write-Host "Port Discovery Monitor -- Monitoring Enabled" -ForegroundColor Green
+
+    Write-Host "- Starting Monitor for 15 Minutes"
+    $XMLFile = Watch-ListeningPorts
+    Write-Host "Log Path: $XMLFile"
+
+    Write-Host "- Parsing and Exporting into CSV"
+    Export-FilteredCSV -FilePath $XMLFile
+
+    Write-Host "- Cleaning Up"
+    Remove-Item -Path $XMLFile -Force
+
+
 }else{
 
     Write-Host "Port Discovery Monitor -- Running Initial Installation" -ForegroundColor Green
 
     # Creating important directories
     Write-Host "- Creating Essential Directories"
-    New-Item -ItemType Directory -Path $Directory -Force
-    New-Item -ItemType Directory -Path $LogDirectory -Force
+    New-Item -ItemType Directory -Path $Directory -Force | Out-Null
+    New-Item -ItemType Directory -Path $LogDirectory -Force | Out-Null
 
     # Downloading CurrPorts
     Write-Host "- Downloading CurrPorts"
-    Get-CurrPorts
+    Get-CurrPorts | Out-Null
 
     # Creating/Updating Scheduled Task
     Write-Host "- Creating/Setting Scheduled Task"
-    Set-ScheduledTask
+    Set-ScheduledTask | Out-Null
 
     # Making a Copy of Itself and Place In Directory
-    Copy-Item -Path $MyInvocation.MyCommand.Path -Destination $ScriptPath -Force
+    Write-Host "- Copying Script into Run Directory"
+    Copy-Item -Path $MyInvocation.MyCommand.Path -Destination $ScriptPath -Force | Out-Null
 
 }
