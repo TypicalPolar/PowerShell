20a08b15ae5d18a8a392ce7909bf4ac0637a1f18
Jose Partida <josepartida.rh@gmail.com>
2026-01-29 21:00:16 -0800

    switched to mkvmerge and finished rest.

--- audio and video/audio-track-missing-language.ps1
+++ audio and video/audio-track-missing-language.ps1
@@ -9,19 +9,56 @@ function Get-AudioTracks {
         throw "Path is invalid or inaccessible"
     }
 
-    return (
-        (MediaInfo --Output=JSON $path | ConvertFrom-Json).media.track |
-        Where-Object { $_."@type" -eq "Audio" }
-    )
+    (mkvmerge -J $Path | ConvertFrom-Json).tracks |
+    Where-Object {
+        $_.type -eq "audio"
+    }
 
 }
 
-$Directory = ""
+function Set-MkvTrackLanguage {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path,
+        [Parameter(Mandatory)]
+        [int]$TrackId,
+        [ValidatePattern('^[a-z]{3}$')]
+        [string]$Language = "eng",
+        [switch]$WhatIf
+    )
+
+    $args = @(
+        $Path,
+        "--edit", "track:@$TrackId",
+        "--set", "language=$Language",
+        "--verbose"
+    )
+
+    if ($WhatIf) {
+        "WHATIF: mkvpropedit $($args -join ' ')"
+        return
+    }
+
+    mkvpropedit @args
+    if ($LASTEXITCODE -ne 0) {
+        throw "mkvpropedit failed: $Path track:$TrackId"
+    }
+}
+
+
+$Directory = "M:\Series\Is It Wrong to Try to Pick Up Girls in a Dungeon\S02\test"
 
 Get-ChildItem -LiteralPath $Directory |
 Where-Object { $_.Extension -like "*mkv*"} |
 Select-Object -ExpandProperty FullName |
 ForEach-Object {
+
+    $File = $_
+
     Get-AudioTracks -Path $_ |
-    Where-Object { -not $_.Language }
+    Where-Object {  $_.properties.language -eq "und" -or -not $_.properties.language } |
+    ForEach-Object {
+            Set-MkvTrackLanguage -Path $File -TrackId $_.properties.number
+    }
+
 }
